b5ed112626ef7cff7bf4f3c63cd04d48b94f910c
sriwaugh <sriwaugh0987@gmail.com>
2025-05-31 15:52:23 +0530

    python Program

--- decodeTheMessage.py
+++ decodeTheMessage.py
@@ -1,9 +1,5 @@
 def decodeMessage(key, message):
-        """
-        :type key: str
-        :type message: str
-        :rtype: str
-        """
+        
         alpa = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"," "]
         letters = []
         s = ""
